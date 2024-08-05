7eb1c91c06272f9edb7b6cf722a57184f2a97212
mangesh vispute <mangesh2003vispute@gmail.com>
2024-08-06 00:29:25 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Menu from "./Menu";
+import DashBord from "./DashBord";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>AppJs</h1>
+      <Header />
+      <Menu />
+      <DashBord />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="main-footer">
+        <strong>
+          Copyright © 2014-2021
+          <a href="https://adminlte.io">AdminLTE.io</a>.
+        </strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.2.0
+        </div>
+      </footer>
+      <aside className="control-sidebar control-sidebar-dark">
+        {/* Control sidebar content goes here */}
+      </aside>
+    </div>
+  );
+};
+
+export default Footer;
