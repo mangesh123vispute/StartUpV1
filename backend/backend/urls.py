dd604ef1961d58ec2af532888d4e4c8a7d60bef1
mangesh vispute <mangesh2003vispute@gmail.com>
2024-08-06 22:56:38 +0530

    Frontend: Login and signUp pages are added, Backend: Swagger documentation is added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,7 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
+# swagger added 
 urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
